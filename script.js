dbef54b6e40c85837b4cc7363ee1270a157243cd
Goran SD <g.cvetic23@gmail.com>
2021-04-16 13:14:34 -0700

    added location

--- script.js
+++ script.js
@@ -5,20 +5,19 @@ document.addEventListener('submit', (e) => {
  });
 
 function recordExpenses(){
-    const table = document.getElementById('tableExpenses').getElementsByTagName('tbody')[0]; 
-    const newRow = table.insertRow(0);
-
+    const newTable = document.getElementById('tableExpenses').getElementsByTagName('tbody')[0]; 
+    const newRow = newTable.insertRow(0);
     const amountCharged = document.getElementById('amountCharged').value;
     const amountFormated  = '$' + amountCharged.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 
-    newRow.insertCell(0).innerHTML = document.getElementById('typeOfCharge').value;
-    newRow.insertCell(1).innerHTML = document.getElementById('expenseName').value;
+    newRow.insertCell(0).innerHTML = document.getElementById('location').value;
+    newRow.insertCell(1).innerHTML = document.getElementById('expenseDescription').value;
     newRow.insertCell(2).innerHTML = dateCorrectFormat();
     newRow.insertCell(3).innerHTML = amountFormated;
     newRow.insertCell(4).innerHTML = '<input type="button" value="Delete" onclick="deleteRow(this)" id="delete">'
 
-    document.getElementById('typeOfCharge').value = '';
-    document.getElementById('expenseName').value = '';
+    document.getElementById('location').value = '';
+    document.getElementById('expenseDescription').value = '';
     document.getElementById('dateCharged').value = '';
     document.getElementById('amountCharged').value = '';
 }
