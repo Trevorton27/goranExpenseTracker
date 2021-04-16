23569e40b3b695f13e341d4b54866a9129184e65
Goran SD <g.cvetic23@gmail.com>
2021-04-15 17:25:23 -0700

    version 2.0

--- script.js
+++ script.js
@@ -8,12 +8,13 @@ function recordExpenses(){
     const table = document.getElementById('tableExpenses').getElementsByTagName('tbody')[0]; 
     const newRow = table.insertRow(0);
 
-    console.log(table.length);
+    const amountCharged = document.getElementById('amountCharged').value;
+    const amountFormated  = '$' + amountCharged.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 
     newRow.insertCell(0).innerHTML = document.getElementById('typeOfCharge').value;
     newRow.insertCell(1).innerHTML = document.getElementById('expenseName').value;
-    newRow.insertCell(2).innerHTML = document.getElementById('dateCharged').value;
-    newRow.insertCell(3).innerHTML = document.getElementById('amountCharged').value;
+    newRow.insertCell(2).innerHTML = dateCorrectFormat();
+    newRow.insertCell(3).innerHTML = amountFormated;
     newRow.insertCell(4).innerHTML = '<input type="button" value="Delete" onclick="deleteRow(this)" id="delete">'
 
     document.getElementById('typeOfCharge').value = '';
@@ -27,4 +28,11 @@ function deleteRow (e) {
     document.getElementById("tableExpenses").deleteRow(deleteButton);
 } 
 
-
+function dateCorrectFormat() {
+    const date = document.getElementById('dateCharged').value.split('-');//date([year],[month],[day])
+    const day = date[2];
+    const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    const monthFormated = months[date[1] - 1]; 
+    const dateFormat = monthFormated + ' - ' + day ;
+    return dateFormat;
+}
